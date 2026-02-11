8069cc7bce912421cc7a25c4a47125aaea3f0d1e
Kirill Fedulin <crazzyhd22@gmail.com>
2026-02-11 14:08:36 +0200

    Add files via upload

---
+++ Tsikle.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace tasks
+{
+    public class Tsikle
+    {
+        public string Nimi;
+        public int Vanus;
+
+        public Tsikle() { }
+        public void Task
+    }
+}
