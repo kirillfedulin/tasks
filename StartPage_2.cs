9a082afa717a16d2e2a4e945b0db7bfff0619351
Kirill Fedulin <crazzyhd22@gmail.com>
2026-02-11 14:09:09 +0200

    Add files via upload

---
+++ StartPage_2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks
+{
+
+    public class StartPage_2
+    {
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Massivid ja listid");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Tere, {i + 1}! ");
+            }
+            Console.WriteLine("While:");
+            int j = 0;
+            while (j < 10)
+            {
+                Console.WriteLine($"Tere, {j + 1}");
+                j++;
+            }
+            do
+            {
+                Console.WriteLine($"Tere, {j + 1}!");
+                j--;
+            }
+            while (j != 0);
+
+            arvud = Funktsioonid_2.Täida_massiiv(arvud);
+
+            arvud = Funktsioonid_2.Foreach_abil(arvud);
+
+            //Küsi mis positsioonil muuda element ja mis väärtuseks, muda element ja kuva massiiv
+            Funktsioonid_2.Muuda_Element(int[] arvud);
+
+
+
+        }
+    }
+}
+
