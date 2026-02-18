be11a9af5aeeda8189d42b641259cb98aa5a0d52
Kirill Fedulin <crazzyhd22@gmail.com>
2026-02-18 22:25:57 +0200

    Add files via upload

---
+++ Naidids/isik.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naidis_csharp
+{
+    public class isik
+    {
+        public string Nimi;
+        public int Vanus;
+
+        public Isik(string nimi, int vanus)
+        {
+            Nimi = nimi;
+            Vanus = vanus;
+        }
+        public Isik() { }
+        public void Tervita()
+        {
+            Console.WriteLine($"Tere, minu numi on {Nimi}");
+        }
+        
+    }
+}
