3377edcba4f7d54553dc6e3c51b90cc82faab5fd
Kirill Fedulin <crazzyhd22@gmail.com>
2026-03-18 14:32:37 +0200

    Add Toode class with properties Nimi and Kalorid

---
+++ osa/Toode.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks.osa
+{
+    public class Toode
+    {
+        public string Nimi { get; set; }
+        public int Kalorid { get; set; }
+    }
+}
