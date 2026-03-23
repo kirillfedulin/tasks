51e682447536b94142141705af1167c554000641
Kirill Fedulin <crazzyhd22@gmail.com>
2026-03-23 11:22:08 +0200

    Add Person2 class with personal attributes

---
+++ osa/Person2.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks.osa
+{
+    public class Person2
+    {
+        public string Nimi { get; set; }
+
+        public int Vanus { get; set; }
+
+        public string Sugu { get; set; }
+
+        public int Pikkus { get; set; }
+
+        public int Kaal { get; set; }
+
+        public int Aktiivsustase { get; set; }
+
+    }
+}
